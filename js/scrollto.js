b9eb26940d99e3064f59754404db24a9c398f196
leahg725 <leahgazes@Leahs-MacBook-Pro-2.local>
2021-03-17 18:14:09 -0400

    fixed links

--- js/scrollto.js
+++ js/scrollto.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
             if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
                 $('#menu-center ul li a').removeClass("active");
                 currLink.addClass("active");
+                console.log(currLink);
             }
             else{
                 currLink.removeClass("active");
@@ -22,7 +23,7 @@ $(document).ready(function() {
           var $target = $(this.hash);
           $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
           if ($target.length) {
-            var targetOffset = $target.offset().top - 20;
+            var targetOffset = $target.offset().top + 20;
 
             $('html,body').animate({scrollTop: targetOffset}, 500);
                 return false;
